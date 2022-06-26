c19b5bfc2e2503b2ce994ae825c308eb79eb5c9b
boyuzhang330 <zhang.boyu@sanmedbio.com>
2022-06-27 00:00:09 +0800

    vent_3

--- baseline.py
+++ baseline.py
@@ -5,12 +5,17 @@ from models.Unet_5 import UNet3D as unet5
 from models.Unet import UNet3D as unet
 from models.artery import UNet3D
 from models.vnet import VNet
+from models.vnet_3 import VNet_3
+
 # Baseline
 
 def get_model(conf,args=None):
     if conf['artery']==1:
         if conf['Vnet']==1:
             net = VNet(coord=True,Dmax=args.cubesize[0], Hmax=args.cubesize[1], Wmax=args.cubesize[2])
+        elif conf['Vnet_3']==1:
+            net = VNet_3(coord=True,Dmax=args.cubesize[0], Hmax=args.cubesize[1], Wmax=args.cubesize[2])
+
         else:
             net = UNet3D(in_channels=1, out_channels=args.out_channels, coord=True, \
                 Dmax=args.cubesize[0], Hmax=args.cubesize[1], Wmax=args.cubesize[2])
